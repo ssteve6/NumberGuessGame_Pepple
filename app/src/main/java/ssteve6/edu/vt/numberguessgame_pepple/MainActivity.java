95f7dc2e7f69c36a11923e5c2aabe593e850f15c
Beakal <beakal8@vt.edu>
2015-03-25 18:00:23 -0400

    Trying to get pebble to work

--- app/src/main/java/ssteve6/edu/vt/numberguessgame_pepple/MainActivity.java
+++ app/src/main/java/ssteve6/edu/vt/numberguessgame_pepple/MainActivity.java
@@ -1,18 +1,68 @@
 package ssteve6.edu.vt.numberguessgame_pepple;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.Random;
+import java.util.UUID;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    public static final String TAG = "PebbleProject";
+
+    private Button higherButton;
+    private Button lowerButton;
+    private Button guessNumber;
+    private TextView theGuessView;
+    private TextView numberView;
+
+    private final static UUID PEBBLE_APP_UUID = UUID.fromString("56bcf818-ed35-4f91-bc80-2197e4733e0c");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        higherButton = (Button) findViewById(R.id.higherButton);
+        lowerButton = (Button) findViewById(R.id.lowerButton);
+        guessNumber = (Button) findViewById(R.id.guessButton);
+        theGuessView = (TextView) findViewById(R.id.theGuessView);
+        numberView = (TextView) findViewById(R.id.numberView);
+
+        boolean connected = PebbleKit.isWatchConnected(getApplicationContext());
+        Log.i(getLocalClassName(), "Pebble is" + (connected ? "connected" : "not connected"));
+
+        Random rand = new Random();
+        final int number = rand.nextInt(100) + 1;
+
+        numberView.setText(Integer.toString(number));
+
+        guessNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                PebbleDictionary data = new PebbleDictionary();
+                data.addUint8(0, (byte) number);
+                PebbleKit.sendDataToPebble(getApplicationContext(), PEBBLE_APP_UUID, data);
+
+                guessNumber.setText("Correct!!");
+            }
+        });
+
     }
+
 
 
     @Override
